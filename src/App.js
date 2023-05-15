da206151690370891909dac7a855c8fe29d4da34
Myeong A Kim <113976428+myeongakim7@users.noreply.github.com>
2023-05-15 18:59:37 +0900

    AOS 설치 후 스크롤 애니메이션 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-//리액트는 싱글페이지 방식으로 페이지가 하나임, 일반 홈페이지 처럼 여러 페이지로 경로를 나누려면 라우팅 처리를 하여야 함
-// 라우팅이란 특정 URL로 요청하면 해당 페이지를 연결해 주는 기능, 각각에 해당하는 페이지를 컴포넌트로 만들어 연결하면 됨
+import logo from "./logo.svg";
+import AOS from "aos";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-//라우터에 연결할 페이지
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  // AOS 초기화
+  useEffect(() => {
+    AOS.init();
+  });
+
+  // npm i aos 설치 후 시작
+  // aos-init = 초기값
+  // aos-animate = 시작
+
+  // 애니메이션 설정한 것들을 class="aos-init aos-animate" 값이 자동으로 생성된다 (개발자도구에서 볼 수 있음)
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <section className="s1">
+        <div>
+          <h1>Animation on Scroll</h1>
+          <p>스크롤 시 애니메이션 효과 주기</p>
+        </div>
+      </section>
+      <section className="s2">
+        <div>
+          <h1 data-AOS="fade-left">Section 2</h1>
+          {/* data-AOS라는 속성 */}
+          <p>김명아</p>
+        </div>
+      </section>
+      <section className="s3">
+        <div>
+          <h1 data-AOS="fade-right">Section 3</h1>
+          <img
+            className="logo"
+            data-AOS
+            data-AOS-delay="0"
+            data-AOS-duration="3000"
+            src={logo}
+            alt="logo"
+          />
+        </div>
+      </section>
+    </div>
   );
 }
 
 export default App;
-
-//리액트 라우터 설치  npm install react-router-dom@6
