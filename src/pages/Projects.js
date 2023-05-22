14c6dcd9c468cd29e63bcb15781ef2750361bcec
Myeong A Kim <113976428+myeongakim7@users.noreply.github.com>
2023-05-22 15:51:02 +0900

    Projects) aos 스크롤 애니메이션 추가

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -59,7 +59,17 @@ function Projects() {
       <Nav />
       <div id="project">
         <div className="container">
-          <section className="itemList">
+          <section
+            className="itemList"
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
+            data-aos-anchor-placement="top-center"
+          >
             <div className="itemBox">
               <Images
                 style={ImgStyle1}
